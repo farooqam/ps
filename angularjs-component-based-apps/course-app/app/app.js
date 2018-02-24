c5aea81a7eb75508e542bb10b83310c5a01c325c
Farooq Mahmud <farooqm@techniqly.com>
2018-02-23 17:08:41 -0800

    Debug helper that puts borders and names around each component.

--- angularjs-component-based-apps/course-app/app/app.js
+++ angularjs-component-based-apps/course-app/app/app.js
@@ -2,13 +2,39 @@
   'use strict';
 
   // Declare app level module which depends on views, and components
-  angular.module('rootModule', [
+  var rootModule = angular.module('rootModule', [
     'ngRoute'
-  ]).
-    config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+  ]);
+
+
+  rootModule.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
       $locationProvider.hashPrefix('!');
 
       $routeProvider.otherwise({ redirectTo: '/' });
     }]);
+
+  rootModule.value('componentBorders', true);
+
+  rootModule.run(function(componentBorders) {
+      if (componentBorders) {
+        if (rootModule._invokeQueue) {
+          rootModule._invokeQueue.forEach(function (item) {
+                if (item[1] == 'component') {
+                    var componentName = item[2][0];
+                    var componentProperties = item[2][1];
+                    if (componentProperties.templateUrl) {
+                        var templateUrl = componentProperties.templateUrl;
+                        delete componentProperties.templateUrl;
+                        componentProperties.template = '<div class="debug-component-borders"><b class="debug-component-name">' + componentName + '</b><div ng-include="\'' + templateUrl + '\'"></div></div>';
+                    }
+                    else {
+                        var template = '<div class="component-borders">' + componentName + '<div>' + componentProperties.template + '</div></div>';
+                        componentProperties.template = template;
+                    }
+                }
+            });
+        }
+    }
+    });
 })();
 
