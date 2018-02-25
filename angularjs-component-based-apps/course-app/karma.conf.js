6d92a0899d6348782ab8861efeefd0484dc4f971
farooqam <farooq_am@hotmail.com>
2018-02-24 17:16:16 -0800

    routing and phantomjs

--- angularjs-component-based-apps/course-app/karma.conf.js
+++ angularjs-component-based-apps/course-app/karma.conf.js
@@ -6,21 +6,35 @@ module.exports = function(config) {
 
     files: [
       'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-route/angular-route.min.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'components/**/*.js',
-      'view*/**/*.js'
+      'app.js',
+      'course/**/*.js',
+      'authorList/**/*.js',      
+      'courseDescription/**/*.js',
+      'courseAuthorInfo/**/*.js',  
+      'courseDiscussion/**/*.js',   
+      'courseHeading/**/*.js', 
+      'courseList/**/*.js', 
+      'courseModules/**/*.js',      
+      'courseNavigator/**/*.js',    
+      'courseViewer/**/*.js',    
+      'home/**/*.js',    
+      'navigator/**/*.js',   
+      'userStatus/**/*.js'
     ],
 
     autoWatch: true,
 
     frameworks: ['jasmine'],
 
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
     plugins: [
       'karma-chrome-launcher',
       'karma-firefox-launcher',
+      'karma-phantomjs-launcher',
       'karma-jasmine',
       'karma-junit-reporter'
     ],
