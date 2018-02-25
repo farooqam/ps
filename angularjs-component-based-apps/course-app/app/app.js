b523ca31d57ed252008eedc40f4433d32d980b6f
Farooq Mahmud <farooqm@techniqly.com>
2018-02-24 16:48:23 -0800

    routing

--- angularjs-component-based-apps/course-app/app/app.js
+++ angularjs-component-based-apps/course-app/app/app.js
@@ -1,40 +1,65 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  // Declare app level module which depends on views, and components
-  var rootModule = angular.module('rootModule', [
-    'ngRoute'
-  ]);
+    // Declare app level module which depends on views, and components
+    var rootModule = angular.module('rootModule', [
+        'ui.router'
+    ]);
 
 
-  rootModule.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
-      $locationProvider.hashPrefix('!');
+    rootModule.config(['$stateProvider', function ($stateProvider) {
+        var states = [
+            {
+                name: 'home',
+                url: '',
+                template: '<home></home>'
+            },
+            {
+                name: 'home2',
+                url: '/',
+                template: '<home></home>'
+            },
+            {
+                name: 'courses',
+                url: '/courses',
+                template: '<course-list></course-list>'
+            },
+            {
+                name: 'authors',
+                url: '/authors',
+                template: '<author-list></author-list>'
+            }
+        ];
 
-      $routeProvider.otherwise({ redirectTo: '/' });
+        states.forEach(function(state) {
+            $stateProvider.state(state);
+        });
+        
+
     }]);
 
-  rootModule.value('componentBorders', true);
+    rootModule.value('componentBorders', true);
 
-  rootModule.run(function(componentBorders) {
-      if (componentBorders) {
-        if (rootModule._invokeQueue) {
-          rootModule._invokeQueue.forEach(function (item) {
-                if (item[1] == 'component') {
-                    var componentName = item[2][0];
-                    var componentProperties = item[2][1];
-                    if (componentProperties.templateUrl) {
-                        var templateUrl = componentProperties.templateUrl;
-                        delete componentProperties.templateUrl;
-                        componentProperties.template = '<div class="debug-component-borders"><b class="debug-component-name">' + componentName + '</b><div ng-include="\'' + templateUrl + '\'"></div></div>';
+    rootModule.run(function (componentBorders) {
+        if (componentBorders) {
+            if (rootModule._invokeQueue) {
+                rootModule._invokeQueue.forEach(function (item) {
+                    if (item[1] == 'component') {
+                        var componentName = item[2][0];
+                        var componentProperties = item[2][1];
+                        if (componentProperties.templateUrl) {
+                            var templateUrl = componentProperties.templateUrl;
+                            delete componentProperties.templateUrl;
+                            componentProperties.template = '<div class="debug-component-borders"><b class="debug-component-name">' + componentName + '</b><div ng-include="\'' + templateUrl + '\'"></div></div>';
+                        }
+                        else {
+                            var template = '<div class="component-borders">' + componentName + '<div>' + componentProperties.template + '</div></div>';
+                            componentProperties.template = template;
+                        }
                     }
-                    else {
-                        var template = '<div class="component-borders">' + componentName + '<div>' + componentProperties.template + '</div></div>';
-                        componentProperties.template = template;
-                    }
-                }
-            });
+                });
+            }
         }
-    }
     });
 })();
 
